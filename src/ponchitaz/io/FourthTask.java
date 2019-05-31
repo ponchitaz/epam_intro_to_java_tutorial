f5ef2de8121933cb9ebdff44360c9507f327921d
Polina Belykh <p.belyh@socialquantum.com>
2019-05-31 14:49:53 +0300

    1 block 3 & 4 tasks drafts

--- src/ponchitaz/io/FourthTask.java
+++ src/ponchitaz/io/FourthTask.java
@@ -1,4 +1,62 @@
 package ponchitaz.io;
 
+// Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях).
+// Поменять местами дробную и целую части числа и вывести полученное значение числа.
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class FourthTask {
+    public static void main(String[] args) {
+        //String rawInput;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a number like nnn.bbb: ");
+            String rawInput = scanner.nextLine();
+            System.out.println("So the number is: " + rawInput);  //test the input
+
+            //List<String> firstPart = new ArrayList<>(Arrays.asList(rawInput.split(".")));
+            String[] parsedArray = rawInput.split(".");
+
+            System.out.println(parsedArray);
+
+            //System.out.println("After the magic is done we get: " + parsedArray[1] + "." + parsedArray[0]);
+            //System.out.println("After the magic is done we get: " + Array.get(parsedArray, 1) + "." + Array.get(parsedArray, 0));
+
+            //char[] inputAsList = rawInput.toCharArray();
+            //ArrayList<String> inputAsList = new ArrayList<String>();
+            //for element in rawInput
+
+            //int thatDouble = rawInput.length();
+
+            //char[] firstPart;
+            //char[] secondPart;
+//            int i, j;
+//
+//            StringBuilder firstPart=new StringBuilder();
+//
+//            for (i = 0; i < thatDouble; i++) {
+//                for (char i:inputAsList){
+//                    while (inputAsList[i] != '.'){
+//                        firstPart.append(inputAsList[i]);
+//                    }
+//                else {
+//                    break;
+//                }
+            }
+//            for (j = thatDouble + 1; j < thatDouble; j--) {
+//                if (b[j] != '.'){
+//                    char[] secondPart.add(b[j]);
+//                }
+//                else {
+//                    break;
+//                }
+//            }
+
+            //double d = Double.parseDouble(b);
+        //}
+
+    }
 }
--- src/ponchitaz/io/ThirdTask.java
+++ src/ponchitaz/io/ThirdTask.java
@@ -1,4 +1,21 @@
 package ponchitaz.io;
 
+//Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+//(𝑠𝑖𝑛 𝑥 + 𝑐𝑜𝑠 𝑦) / (𝑐𝑜𝑠 𝑥 − 𝑠𝑖𝑛 𝑦) ∗ 𝑡𝑔(𝑥𝑦)
+
+import java.util.Scanner;
+
 public class ThirdTask {
+    public static void main(String[] args) {
+        double x, y;
+        try (Scanner scanner = new Scanner(System.in)) {
+            String newLine = System.getProperty("line.separator");
+            System.out.println("Here is another one: (sinX + cosY) / (cosX - sinY) * tg(x * y)" + newLine + "What numbers for this one?" + newLine + "For X: ");
+            x = Double.parseDouble(scanner.nextLine()); //parsing a string from user input to get a double
+            System.out.println("Now for Y: ");
+            y = Double.parseDouble(scanner.nextLine());
+            double z = ((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) * Math.tan(Math.toRadians(x * y));
+            System.out.println("And the answer is: " + z);
+        }
+    }
 }
